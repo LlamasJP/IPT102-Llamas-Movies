d0820dff1e2c414aa7a45a723dae5e4b7213000a
LlamasJP <llamas.johnpaul.12082000@gmail.com>
2023-10-10 14:09:04 +0800

    add parameters & variables

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -6,9 +6,15 @@ namespace BSIT3L_Movies.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Rating { get; set; }
+        public int Rating { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
+        public String Cover { get; set; }
+        public string Link { get; set; }
+        public string Star {  get; set; }
+        public string Content { get; set; }
+
+        //https://github.com/ahmedtoba/Movie_Ticket_Reservation/blob/main/MovieTickets/Views/Home/Index.cshtml
         
     }
 }
